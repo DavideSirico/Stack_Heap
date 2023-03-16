07ee94210efaa594b2784867c0da0456f9583af2
DavideSirico <davide.sirico@gmail.com>
2023-03-16 21:45:24 +0100

    Add initial project files and code

---
+++ untitled/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 5;
+        int b = 10;
+        int somma = somma(a, b);
+        System.out.println(somma);
+    }
+    public static int somma(int a, int b) {
+        int somma = a + b;
+        return somma;
+    }
+}
