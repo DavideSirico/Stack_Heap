eb8be317456e407cb4990e136675b3b29ae57a49
DavideSirico <davide.sirico@gmail.com>
2023-03-17 23:32:08 +0100

    Update JDK version to 11 and print process ID in Main class

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long pid = ProcessHandle.current().pid();
+        System.out.println(pid);
         int a = 5;
         int b = 10;
         int somma = somma(a, b);
