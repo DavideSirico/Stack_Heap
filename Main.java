45d0189b12493bff8ce7a63f1f4c8ee45d59858c
Davide Sirico <davide.sirico@gmail.com>
2023-03-22 19:40:32 +0100

    daje roma dajeeeeeeee

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		Integer b = new Integer(1);
+		foo();
+		System.out.println("Hello World!");
+		while(true);
+	}
+	public static void foo()
+	{
+		int x = 5;
+		int y = 12;
+	}
+}
+
